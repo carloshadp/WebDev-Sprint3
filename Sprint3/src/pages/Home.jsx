26505abb9b81143de4d134940d5a1aa470f918c7
Rodrigo Hiroshi <161664438+Hirou-shi@users.noreply.github.com>
2024-09-20 22:35:15 -0300

    aaa

--- Sprint3/src/pages/Home.jsx
+++ Sprint3/src/pages/Home.jsx
@@ -1,6 +1,18 @@
+/*
+width: w
+paddind: p
+hr
+
+
+
+*/
+
+
 
 export default function Home(){
     return(
+        <body class="h-svh">
         <h1> Home Page</h1>
+        </body>
     )
 }
--- Sprint3/tailwind.config.js
+++ Sprint3/tailwind.config.js
@@ -5,7 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      'menu-imagem': "url('/src/assets/Menu.svg')"
+    },
   },
   plugins: [],
 }
