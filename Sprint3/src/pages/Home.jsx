126d9b50009769c1cbc4715c5123a49c9a1e2a93
Rodrigo Hiroshi <rodrigohiroshiwatanabe@gmail.com>
2024-09-28 22:27:47 -0300

    a

--- Sprint3/src/pages/Home.jsx
+++ Sprint3/src/pages/Home.jsx
@@ -2,8 +2,8 @@ import Corrida from "/src/components/Corrida.jsx"
 
 export default function Home(){
     return(
-        <body class="sm:h-svh sm:m-28">
-            <section>
+        <body class="h-svh sm:m-28">
+            <section class="top-11">
                 <h1>Quem somos?</h1>
                 <p>Site oficial do Formula-E Manager, um site dedicado aos nossos players. <br/>Aqui, fornecemos informacoes que podem ser importantes para os jogadores tomarem decisoes no jogo, tanto times com a melhor performance, quanto pilotos e os circuitos claro, para estudarem a pista.</p>
             </section>
